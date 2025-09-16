795a8aa3e740cb2de119aac6cb78c1b65cf566d7
Daniel Gorgonha <rogergorgonha@gmail.com>
2025-09-16 12:32:13 -0300

    fix

--- frontend/app/(auth)/register/page.tsx
+++ frontend/app/(auth)/register/page.tsx
@@ -1,6 +1,6 @@
 'use client'
 
-import { RegisterForm } from '../../components/features/auth/RegisterForm'
+import { RegisterForm } from '../../../components/features/auth/RegisterForm'
 
 export default function RegisterPage() {
   return (
